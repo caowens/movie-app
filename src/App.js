8cfa6bd911dc123d35665caa6ddf488b8d107c17
caowens <caowens555@yahoo.com>
2022-10-03 23:49:51 -0500

    saving and recieving from local storage

--- src/App.js
+++ src/App.js
@@ -27,17 +27,31 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(() => {
+    const MovieFavourites = JSON.parse(
+      localStorage.getItem("react-movie-app-favourites")
+    );
+
+    setFavourites(MovieFavourites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
+  };
+
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
 
-  const RemoveFavouriteMovie = (movie) => {
+  const removeFavouriteMovie = (movie) => {
     const newFavouriteList = favourites.filter(
       (favourite) => favourite.imdbID !== movie.imdbID
     );
 
     setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
 
   return (
@@ -59,7 +73,7 @@ const App = () => {
       <div className="row">
         <MovieList
           movies={favourites}
-          handleFavouritesClick={addFavouriteMovie}
+          handleFavouritesClick={removeFavouriteMovie}
           favouriteComponent={RemoveFavourites}
         />
       </div>
